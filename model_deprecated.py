284625f6119752c10f62a528c05cb5841553c3dd
noacaspi <nocaspi@mail.tau.ac.il>
2025-04-14 12:25:03 +0300

    change req

--- model_deprecated.py
+++ model_deprecated.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-from torch.nn import MultiheadAttention
 
 from braid_env import BraidEnvironment
 
